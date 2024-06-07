1a112ba6f77b2d6929c26882da79abf7690a595b
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-06-06 19:34:47 -0700

    Add files via upload

---
+++ Class-2/main.js
@@ -0,0 +1,18 @@
+var name1 = "Muhammad";
+console.log(name1);
+var name2 = "Ali";
+console.log(name2);
+var color = "blue";
+console.log(color);
+var age = 5;
+console.log(age);
+var car = "newcar";
+var car = "oldcar";
+console.log(car);
+var myname = "warsa"; //string
+var age = 17; //number
+var ismarried = false; //boolean
+var randomvalue = undefined; //undefined, non-existing
+var newvalue = null; //can be updated later
+myname = "warsa";
+myname = 1223;
---
+++ Class-2/main.ts
@@ -0,0 +1,24 @@
+let name1 = "Muhammad"
+console.log(name1);
+
+let name2 = "Ali"
+console.log(name2);
+
+const color = "blue"
+console.log(color);
+
+let age = 5
+console.log(age);
+
+var car = "newcar"
+var car = "oldcar"
+console.log(car);
+
+let myname = "warsa" //string
+let age : number = 17 //number
+let ismarried : boolean = false //boolean
+let randomvalue : undefined = undefined //undefined, non-existing
+let newvalue : null = null //can be updated later
+
+myname = "warsa"
+myname = 1223
